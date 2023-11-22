b0a5d7497e7bb36a244b80181797a4d372d03839
pho-dev <pholiveira.dev@gmail.com>
2023-11-22 20:00:33 -0300

    Projeto sobre Context API

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import Routes from "./src/routes";
+import AuthProvider from "./contexts/auth"; 
+
+//import Login from "./src/pages/Login";
+//import Home from "./src/pages/Home";
+//import Pedidos from "./src/pages/Pedidos";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ readme.md
@@ -0,0 +1,18 @@
+
+# Projeto para testes sobre ContextApi 
+
+Base do projeto para entendimento de contextapi. Esse projeto foi criado com EXPO.
+
+
+## Instalação
+
+Clone esse repositório e instale as dependencias do projeto:
+
+```bash
+  npm install
+```
+    
+Depois rode o projeto:
+```bash
+  npx expo start
+```
